311707c0cfeef79a7b1a26e2908e80aca4e5b175
LutfiB <lutfibagasw93@gmail.com>
2025-09-24 10:41:15 +0700

    feature: update HomeButton and Image in GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ export function GameCard({game}: {game : Game}) {
   <div className="bg-gradient-to-br from-purple-700/60 via-violet-800/50 to-blue-800/60 p-4 rounded-xl">
     <div className="mb-3">
       <Image src={game.background_image || "/placeholder.jpg"} 
-      alt="" width={500} height={500} loading="lazy"className={game.background_image? "rounded-lg object-cover": "rounded-lg bg-gray-800 p-1.5"}></Image>
+      alt="" width={500} height={500} loading="lazy" quality={90} className={game.background_image? "rounded-lg object-cover": "rounded-lg bg-gray-800 p-1.5"}></Image>
     </div>
     <div className="space-y-1 pb-4.5">
       <h3 className="text-center font-semibold">{game.name}</h3>
@@ -21,7 +21,7 @@ export function GameCard({game}: {game : Game}) {
       <p className="text-center">Last Updated: {formatDate(game.updated)}</p>
     </div>
     <div className="text-center">
-      <Link href={`/game/${game.id}`} className="bg-sky-500/60 p-2.5 rounded-xl hover:bg-sky-400/90" >Detail</Link>
+      <Link href={`/game/${game.id}`} className="bg-sky-600/60 p-2.5 rounded-xl hover:bg-sky-500/90" >Detail</Link>
     </div>
   </div>
   )
--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -5,6 +5,6 @@ export function HomeButton(){
   const router = useRouter();
   return(
     <button onClick={() => router.push("/")}
-    className="p-1.5 bg-violet-700 absolute top-3 left-4 cursor-pointer hover:bg-violet-800">Home</button>
+    className="p-1.5 bg-violet-700 absolute top-3 left-4 cursor-pointer hover:bg-violet-800 rounded-xl hover:scale-105">Home</button>
   )
 }
