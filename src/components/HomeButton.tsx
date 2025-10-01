0cbb6049356e97f6afcdb064c940943a71ba1287
LutfiB <lutfibagasw93@gmail.com>
2025-10-01 11:21:19 +0700

    change button in homebutton

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -12,7 +12,7 @@ export function HomeButton(){
     setTimeout(() => {setDisabled(false)}, 500)
   }
   return(
-    <button onClick={() => router.push("/")}
+    <button onClick={() => handleHome()}
     className="p-1.5 bg-violet-700 absolute top-3 left-4 cursor-pointer hover:bg-violet-800 rounded-xl hover:scale-105">Home</button>
   )
 }
