88df10864a6c1ef45486133c63add2e4dc700dff
LutfiB <lutfibagasw93@gmail.com>
2025-10-09 15:03:50 +0700

    feat: update GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -19,6 +19,7 @@ export function GameCard({game}: {game : Game}) {
       <p className="text-center">Released: {game.released?? "To Be Announced"}</p>
       <p className="text-center">Ratings: {game.ratings[0]?.percent}</p>
       <p className="text-center">Last Updated: {formatDate(game.updated)}</p>
+      <p className="text-center">Genre: {game.genres[1]?.name}</p>
     </div>
     <div className="text-center">
       <Link href={`/game/${game.id}`} className="bg-sky-600/60 p-2.5 rounded-xl hover:bg-sky-500/90" >Detail</Link>
