a24f458b622754c827138a6401b298330d0ac416
LutfiB <lutfibagasw93@gmail.com>
2026-02-03 10:38:49 +0700

    style(ui): change color to better ux

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import { HomeButton } from "@/components/HomeButton";
 export default function LoginPage() {
   return (
     <>
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-blue-700 via-indigo-700  to-purple-700 text-white">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-blue-800 via-indigo-800  to-purple-800 text-white">
     <HomeButton/>
       <h1 className="text-3xl font-bold mb-6"></h1>
       <LoginForm />
