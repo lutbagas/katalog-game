8e4c10ce8503a0bf225cec0997a0fba362d0347d
LutfiB <lutfibagasw93@gmail.com>
2026-02-18 13:31:15 +0700

    style(ui): adjust login card to better ux

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -52,42 +52,46 @@ export function LoginForm(){
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-gray-950 p-9 space-y-5 text-indigo-100">
-      <h3 className="text-2xl text-center font-semibold">Login</h3>
+    <div className="min-h-screen flex items-center justify-center px-120">
 
-      <input 
-        type="email"
-        value={email}
-        placeholder="Masukkan email"
-        autoComplete="new-email"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        className="bg-gray-800 w-full p-3 focus:ring-2 outline-none focus:ring-violet-500"
-      />
+      <form onSubmit={handleSubmit} className="bg-gray-950 p-9 space-y-5 text-indigo-100">
+        <h3 className="text-2xl text-center font-semibold">Login</h3>
 
-      <input 
-        type="password"
-        value={password}
-        placeholder="Masukkan password"
-        autoComplete="new-password"
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        className="bg-gray-800 w-full p-3 focus:ring-2 outline-none focus:ring-violet-500"
-      />
+        <input 
+          type="email"
+          value={email}
+          placeholder="Masukkan email"
+          autoComplete="new-email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="bg-gray-800 w-full p-3 focus:ring-2 outline-none focus:ring-violet-500"
+        />
 
-      <button 
-        className={`p-2 w-full transition-colors hover:bg-purple-800 hover:scale-105 ${loading ? "bg-gray-700" : "bg-violet-900 cursor-pointer"}`}
-        disabled={loading}
-      >
-        {loading ? "Memproses..." : "Masuk"}
-      </button>
+        <input 
+          type="password"
+          value={password}
+          placeholder="Masukkan password"
+          autoComplete="new-password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="bg-gray-800 w-full p-3 focus:ring-2 outline-none focus:ring-violet-500"
+        />
 
-      {loading && <PiSpinnerGapBold className="ml-2 animate-spin" />}
-      {message.text && (
-        <p className={message.type === "success" ? "text-green-500" : "text-red-500"}>
-          {message.text}
-        </p>
-      )}
-    </form>
+        <button 
+          className={`p-2 w-full transition-colors hover:bg-purple-800
+             ${loading ? "bg-gray-700" : "bg-violet-900 cursor-pointer"}`}
+          disabled={loading}
+        >
+          {loading ? "Memproses..." : "Masuk"}
+        </button>
+
+        {loading && <PiSpinnerGapBold className="ml-2 animate-spin" />}
+        {message.text && (
+          <p className={message.type === "success" ? "text-green-500" : "text-red-500"}>
+            {message.text}
+          </p>
+        )}
+      </form>
+    </div>
   );
 }
