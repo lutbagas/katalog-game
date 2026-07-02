5439c359effd838387d1f20b008b61981f58a2c9
LutfiB <lutfibagasw93@gmail.com>
2026-07-02 12:57:15 +0700

    style: add button home in loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
 // app/login/page.tsx
+'use client'
 import { LoginForm } from "@/components/LoginForm";
 import { Suspense } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter()
   return (
     <Suspense
       fallback={
@@ -11,6 +14,8 @@ export default function LoginPage() {
         </div>
       }
     >
+      <button className="bg-linear-to-br from-purple-600 via-purple-600 to to-purple-900 mx-5 my-10 rounded-xl px-2 py-2 cursor-pointer"
+      onClick={() => router.push('/')}>&larr; home</button>
       <LoginForm />
     </Suspense>
   );
